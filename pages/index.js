2a270c31199eac430548cdbd68ce7a7b7b04b6a4
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-16 23:02:58 +0200

    adding ResourcePicker component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import { Page } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
 
-const Index = () => (
-  <Page
-    title="Page title"
-    primaryAction={{ content: "Click me", onAction: (e) => console.log(e) }}
-  />
-);
+class Index extends React.Component {
+  state = { open: false };
+  render = () => (
+    <Page
+      title="Page title"
+      primaryAction={{
+        content: "Click me",
+        onAction: (e) => this.setState({ open: true }),
+      }}
+    >
+      <ResourcePicker resourceType="Product" open={this.state.open} />
+    </Page>
+  );
+}
 
 export default Index;
