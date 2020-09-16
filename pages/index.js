8fe393194f30255d834a18ebdc9c34da15c6b4b8
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-17 00:09:43 +0200

    adding handleSelection function

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,26 @@ import { ResourcePicker } from "@shopify/app-bridge-react";
 
 class Index extends React.Component {
   state = { open: false };
+
+  handleSelection = (resources) => {
+    const ids = resources.selection.map((e) => e.id);
+    this.setState({ open: false });
+    console.log(ids);
+  };
   render = () => (
     <Page
       title="Page title"
       primaryAction={{
         content: "Click me",
-        onAction: (e) => this.setState({ open: true }),
+        onAction: () => this.setState({ open: true }),
       }}
     >
-      <ResourcePicker resourceType="Product" open={this.state.open} />
+      <ResourcePicker
+        resourceType="Product"
+        open={this.state.open}
+        onCancel={() => this.setState({ open: false })}
+        onSelection={(resources) => this.handleSelection(resources)}
+      />
     </Page>
   );
 }
