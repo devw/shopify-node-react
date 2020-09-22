e91a101f36ecfb18c042bc4cea144a341f4fa406
Antonio Pierro <antonio.pierro@gmail.com>
2020-09-22 16:05:12 +0200

    addding scriptTags/handleAccount.js file

---
+++ scriptTags/handleAccount.js
@@ -0,0 +1,8 @@
+const handleAccountClick = (e) => {
+  e.preventDefault();
+  console.log("hello2");
+};
+
+document
+  .querySelector(".site-header__account")
+  .addEventListener("click", handleAccountClick);
